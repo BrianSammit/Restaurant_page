d06350160287fecf2cff989d75423438c3ae7a3c
BrianSammit <briansammit.cruz@gmail.com>
2021-03-12 14:04:02 -0500

    Fix funtions to use ES6 syntax

--- src/home.js
+++ src/home.js
@@ -1,28 +1,28 @@
-function createHome() {
-  const home = document.createElement('div');
-  home.className = 'home';
+const createHome = () => {
+  const home = document.createElement("div");
+  home.className = "home";
 
-  const img = document.createElement('img');
-  img.src = 'img/chef.jpg';
-  img.alt = 'Chef';
+  const img = document.createElement("img");
+  img.src = "img/chef.jpg";
+  img.alt = "Chef";
 
-  const h2 = document.createElement('h2');
-  h2.innerText = 'Happy Belly Happy Life';
+  const h2 = document.createElement("h2");
+  h2.innerText = "Happy Belly Happy Life";
 
-  const p2 = document.createElement('p');
-  p2.innerText = 'Get your favorite food and enjoy it';
+  const p2 = document.createElement("p");
+  p2.innerText = "Get your favorite food and enjoy it";
 
   home.appendChild(h2);
   home.appendChild(img);
   home.appendChild(p2);
 
   return home;
-}
+};
 
-function loadHome() {
-  const main = document.getElementById('main');
-  main.textContent = '';
+const loadHome = () => {
+  const main = document.getElementById("main");
+  main.textContent = "";
   main.appendChild(createHome());
-}
+};
 
 export default loadHome;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
-import './style.scss';
-import loadHome from './home';
-import loadMenu from './menu';
-import loadContact from './contact';
+import "./style.scss";
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
 
-function setActiveButton(button) {
-  const buttons = document.querySelectorAll('.button-nav');
+const setActiveButton = (button) => {
+  const buttons = document.querySelectorAll(".button-nav");
 
   buttons.forEach((button) => {
     if (button !== this) {
-      button.classList.remove('active');
+      button.classList.remove("active");
     }
   });
 
-  button.classList.add('active');
-}
+  button.classList.add("active");
+};
 
-function createNav() {
-  const nav = document.createElement('nav');
+const createNav = () => {
+  const nav = document.createElement("nav");
 
-  const hh = document.createElement('button');
-  hh.className = 'button-nav';
-  hh.textContent = 'Home';
-  hh.addEventListener('click', (e) => {
-    if (e.target.classList.contains('active')) return;
+  const hh = document.createElement("button");
+  hh.className = "button-nav";
+  hh.textContent = "Home";
+  hh.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
     setActiveButton(hh);
     loadHome();
   });
 
-  const hm = document.createElement('button');
-  hm.className = 'button-nav';
-  hm.textContent = 'Menu';
-  hm.addEventListener('click', (e) => {
-    if (e.target.classList.contains('active')) return;
+  const hm = document.createElement("button");
+  hm.className = "button-nav";
+  hm.textContent = "Menu";
+  hm.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
     setActiveButton(hm);
     loadMenu();
   });
 
-  const hc = document.createElement('button');
-  hc.className = 'button-nav';
-  hc.textContent = 'Contact';
-  hc.addEventListener('click', (e) => {
-    if (e.target.classList.contains('active')) return;
+  const hc = document.createElement("button");
+  hc.className = "button-nav";
+  hc.textContent = "Contact";
+  hc.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
     setActiveButton(hc);
     loadContact();
   });
@@ -50,20 +50,20 @@ function createNav() {
   nav.appendChild(hc);
 
   return nav;
-}
+};
 
-function createHeader() {
-  const header = document.createElement('header');
-  header.className = 'header';
+const createHeader = () => {
+  const header = document.createElement("header");
+  header.className = "header";
 
-  const div = document.createElement('div');
+  const div = document.createElement("div");
 
-  const h1 = document.createElement('h1');
-  h1.className = 'restaurant-name';
-  h1.innerText = 'GoGrill';
+  const h1 = document.createElement("h1");
+  h1.className = "restaurant-name";
+  h1.innerText = "GoGrill";
 
-  const span = document.createElement('span');
-  span.innerText = '!!';
+  const span = document.createElement("span");
+  span.innerText = "!!";
 
   div.appendChild(h1);
   div.appendChild(span);
@@ -71,40 +71,40 @@ function createHeader() {
   header.appendChild(createNav());
 
   return header;
-}
+};
 
-function createMain() {
-  const main = document.createElement('main');
-  main.classList.add('main');
-  main.setAttribute('id', 'main');
+const createMain = () => {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
   return main;
-}
+};
 
-function createFooter() {
-  const footer = document.createElement('footer');
-  footer.className = 'footer';
+const createFooter = () => {
+  const footer = document.createElement("footer");
+  footer.className = "footer";
 
-  const pf = document.createElement('p');
-  pf.innerText = 'Copyright © 2021 Brian ';
+  const pf = document.createElement("p");
+  pf.innerText = "Copyright © 2021 Brian ";
 
-  const icong = document.createElement('i');
-  icong.className = 'fab fa-github';
+  const icong = document.createElement("i");
+  icong.className = "fab fa-github";
 
   pf.appendChild(icong);
   footer.appendChild(pf);
 
   return footer;
-}
+};
 
-function initializeWebsite() {
-  const content = document.getElementById('content');
+const initializeWebsite = () => {
+  const content = document.getElementById("content");
 
   content.appendChild(createHeader());
   content.appendChild(createMain());
   content.appendChild(createFooter());
 
-  setActiveButton(document.querySelector('.button-nav'));
+  setActiveButton(document.querySelector(".button-nav"));
   loadHome();
-}
+};
 
 initializeWebsite();
